e164d79a030ea1d2f063679a47b4429da47f0ff8
kingRajendra17 <rajendraroccss@gmail.com>
2023-05-07 18:23:06 +0530

    Prime Number

--- src/JavaProblems/PrimeNumber.java
+++ src/JavaProblems/PrimeNumber.java
@@ -6,6 +6,10 @@ public class PrimeNumber {
 	
 	public static void main(String [] args) {
 		
+		// Prime Number Eligibility 
+		// Number > 1
+		// Divide by 1 or itself == 0
+		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Enter a Number : ");	
